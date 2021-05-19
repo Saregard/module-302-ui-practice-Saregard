14879669c8b5ca5751f5bc7c166eab0ea9837698
Saregard <77791326+Saregard@users.noreply.github.com>
2021-05-19 13:31:54 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/uipractice/MainActivity.kt
+++ app/src/main/java/com/example/uipractice/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.uipractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import org.w3c.dom.Text
+import java.text.DateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val calendar = Calendar.getInstance()
 
+        val currentDate = DateFormat.getDateInstance().format(calendar.time)
 
-
+        val textViewDate = findViewById<TextView>(R.id.text_view_date)
+        textViewDate.text = currentDate
     }
 }
