932f38ebde3171b4337d8bc6ef02ed19e1cef979
Saregard <77791326+Saregard@users.noreply.github.com>
2021-05-20 12:40:23 +0200

    Update MainActivity2.kt

--- app/src/main/java/com/example/uipractice/MainActivity2.kt
+++ app/src/main/java/com/example/uipractice/MainActivity2.kt
@@ -2,10 +2,16 @@ package com.example.uipractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity2 : AppCompatActivity() {
+
+    private var editTextEmail: EditText? = null
+    private var editTextPassword: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
     }
 }
