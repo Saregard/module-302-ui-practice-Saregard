e7b64b19b4d397031fd86a6ee496f6aae49b1a19
Saregard <77791326+Saregard@users.noreply.github.com>
2021-05-20 12:40:24 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/uipractice/MainActivity.kt
+++ app/src/main/java/com/example/uipractice/MainActivity.kt
@@ -16,8 +16,6 @@ class MainActivity : AppCompatActivity() {
     private var signUpButton: Button? = null
     private var loginButton: Button? = null
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
