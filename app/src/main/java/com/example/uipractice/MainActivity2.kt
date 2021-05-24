3b60ac2aa634f53dfe92ef9539865dd8940c36e7
Saregard <77791326+Saregard@users.noreply.github.com>
2021-05-24 15:35:03 +0200

    Update MainActivity2.kt

--- app/src/main/java/com/example/uipractice/MainActivity2.kt
+++ app/src/main/java/com/example/uipractice/MainActivity2.kt
@@ -2,16 +2,42 @@ package com.example.uipractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.view.KeyEvent
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
-class MainActivity2 : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity(){
 
     private var editTextEmail: EditText? = null
     private var editTextPassword: EditText? = null
+    private var loginButton: Button? = null
+    private var forgotPasswordText: TextView? = null
+    private var facebookLoginButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        editTextEmail = findViewById(R.id.email)
+        editTextPassword = findViewById(R.id.password)
+        loginButton = findViewById(R.id.login)
+        forgotPasswordText = findViewById(R.id.forgot_password)
+        facebookLoginButton = findViewById(R.id.log_in_with_facebook)
+
+        loginButton?.setOnClickListener {
+            if (editTextEmail?.length() == 0){
+                editTextEmail?.error = getString(R.string.username_required_error)
+                true
+            }else if (editTextPassword?.length() == 0) {
+                editTextPassword?.error = getString(R.string.password_required_error)
+                true
+            }else
+                false
+        }
+
     }
+
 }
