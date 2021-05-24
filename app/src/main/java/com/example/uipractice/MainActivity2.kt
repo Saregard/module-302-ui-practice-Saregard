613b43d14c0e7a487d457687a733328265bb002a
Saregard <77791326+Saregard@users.noreply.github.com>
2021-05-24 17:22:08 +0200

    Update MainActivity2.kt

--- app/src/main/java/com/example/uipractice/MainActivity2.kt
+++ app/src/main/java/com/example/uipractice/MainActivity2.kt
@@ -16,10 +16,19 @@ class MainActivity2 : AppCompatActivity(){
     private var loginButton: Button? = null
     private var forgotPasswordText: TextView? = null
     private var facebookLoginButton: Button? = null
+    private val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+        val email:String="asd@gmail.com"
+        if (email.matches(emailPattern.toRegex())){
+            Toast.makeText(this,"Hello ",Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this,"Bye Bye ",Toast.LENGTH_LONG).show()
+        }
 
         editTextEmail = findViewById(R.id.email)
         editTextPassword = findViewById(R.id.password)
@@ -28,14 +37,18 @@ class MainActivity2 : AppCompatActivity(){
         facebookLoginButton = findViewById(R.id.log_in_with_facebook)
 
         loginButton?.setOnClickListener {
-            if (editTextEmail?.length() == 0){
-                editTextEmail?.error = getString(R.string.username_required_error)
-                true
-            }else if (editTextPassword?.length() == 0) {
-                editTextPassword?.error = getString(R.string.password_required_error)
-                true
-            }else
-                false
+            when {
+                editTextEmail?.length() == 0 -> {
+                    editTextEmail?.error = getString(R.string.username_required_error)
+                }
+                editTextPassword?.length() == 0 -> {
+                    editTextPassword?.error = getString(R.string.password_required_error)
+                }
+                editTextEmail?.text.toString() != emailPattern -> {
+                    editTextEmail?.error = getString(R.string.needs_to_be_email)
+                }
+                else -> Toast.makeText(this, "logged in", Toast.LENGTH_LONG).show()
+            }
         }
 
     }
